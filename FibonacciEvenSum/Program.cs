41e7cc7902dec6e87f824f007ac46918c2c1ded5
ram5yc <ram5yc@mail.missouri.edu>
2021-03-10 07:04:08 -0600

    commit

--- FibonacciEvenSum/Program.cs
+++ FibonacciEvenSum/Program.cs
@@ -10,10 +10,20 @@ namespace FibonacciEvenSum
     {
         static void Main(string[] args)
         {
-            for (int i = 0; i < 4000000; i++)
+           
+            int fib1 = 0; //variable for first number
+            int fib2 = 1; //variable for second number
+            int sum = 0; //stores addition
+            do
             {
+                //string to make the output of addition stored in new variable
+                sum = sum + fib1 + fib2;
 
-            }
+                //divide by 2 to make it even numbers only
+                sum = sum / 2;
+
+            } while (sum < 4000000); //variable goes until 4 mill
+            Console.WriteLine(sum);
         }
     }
 }
