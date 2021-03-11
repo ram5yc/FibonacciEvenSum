5be70eface806abf4e083bf6deb3555c278f5c12
ram5yc <ram5yc@mail.missouri.edu>
2021-03-11 08:24:11 -0600

    commit

--- FibonacciEvenSum/Program.cs
+++ FibonacciEvenSum/Program.cs
@@ -4,6 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+//Each new term in the Fibonacci sequence is generated
+//by adding the previous two terms.By starting with
+//1 and 2, the first 10 terms will be:
+
+//1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ...
+//By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
+
 namespace FibonacciEvenSum
 {
     class Program
@@ -11,19 +19,33 @@ namespace FibonacciEvenSum
         static void Main(string[] args)
         {
            
-            int fib1 = 0; //variable for first number
-            int fib2 = 1; //variable for second number
-            int sum = 0; //stores addition
+
+            int fib1 = 1; //variable for first number
+            int fib2 = 2; //variable for second number
+            int fib3 = 0; //stores addition
+
+            int fib4 = 0;
+            
             do
             {
                 //string to make the output of addition stored in new variable
-                sum = sum + fib1 + fib2;
+                fib3 = fib1 + fib2;
+                fib1 = fib2;
+                fib2 = fib3;
+                //fib4 += fib3;
 
-                //divide by 2 to make it even numbers only
-                sum = sum / 2;
 
-            } while (sum < 4000000); //variable goes until 4 mill
-            Console.WriteLine(sum);
+                if (fib2 % 2 == 0)
+                {
+                    fib4 += fib2;
+                }
+
+                
+            } while (fib4 < 4000000); //variable goes until 4 mill
+            Console.WriteLine(fib4);
         }
     }
 }
+
+//divide by 2 to make it even numbers only
+//sum = sum % 2;
